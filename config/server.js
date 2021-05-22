93824cbce267bb728f2d5c089003171974be0bd4
David Noe <info@davidnoe.ch>
2021-05-22 22:35:25 +0200

    Initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '2bb66b7e6333ce1ea227920ff7f33aa9'),
+    },
+  },
+});
